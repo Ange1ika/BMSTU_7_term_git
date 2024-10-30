b19e5c54dfd9015ba32792b7eb871cc779844a8d
Kochetkova Ange1ika <kochetkova.angelika@yandex.ru>
2024-10-30 19:48:17 +0300

    USART

---
+++ ARM_F407_usart/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/core_cm4.h \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/cmsis_version.h \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/cmsis_compiler.h \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/cmsis_gcc.h \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/mpu_armv7.h \
+ C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/core_cm4.h:
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/cmsis_version.h:
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/cmsis_compiler.h:
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/cmsis_gcc.h:
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Include/mpu_armv7.h:
+C:/ARM_Prj/ARM_F407_spi_i2c/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
